52fccc099b1e4dd4e09cdf01281a68d1da149289
Benjamin Ryan Toaz <benjamin_r_toaz@whirlpool.com>
2023-08-02 17:19:52 -0400

    Update robo_poke_gui_draft.py

--- examples/robo_poke_gui_draft.py
+++ examples/robo_poke_gui_draft.py
@@ -1,36 +1,11 @@
-# import customtkinter
-
-# customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-# customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-
-# app = customtkinter.CTk()
-# app.geometry("400x780")
-# app.title("Robo Poke")
-
-# def button_callback():
-#     print("Button click")
-
-# frame_1 = customtkinter.CTkFrame(master=app)
-# frame_1.pack(pady=20, padx=60, fill="both", expand=True)
-
-# frame_2 = customtkinter.CTkFrame(master=app)
-# frame_2.pack(pady=20, padx=60, fill="both", expand=True)
-
-# button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback, text="Start")
-# button_1.pack(pady=10, padx=10)
-
-# button_2 = customtkinter.CTkButton(master=frame_1, command=button_callback, text='Stop')
-# button_2.pack(pady=10, padx=10)
-
-# text_1 = customtkinter.CTkTextbox(master=frame_1, width=200, height=70)
-# text_1.pack(pady=10, padx=10)
-# text_1.insert("0.0", "CTkTextbox\n\n\n\n")
-
-# app.mainloop()
+# build here until ready to add backend
 
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from PIL import Image
+import os
+
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -53,31 +28,44 @@ class App(customtkinter.CTk):
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Robot Control", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Start")
+        
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.start_button_event, text="Start")
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Stop")
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.stop_button_event, text="Stop")
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
-
+        
+        # image frame
+        # load and create background image
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        self.bg_image = customtkinter.CTkImage(Image.open(current_path + "/test_images/bg_gradient.jpg"),
+                                               size=(800, 600))
+        self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image, text="")
+        self.bg_image_label.grid(row=0, column=1, padx=(20, 0), pady=(10, 0), sticky="nsew")
+        
         # create textbox
-        self.textbox = customtkinter.CTkTextbox(self, width=250)
-        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
+        self.textbox = customtkinter.CTkTextbox(self, width=200, height=200)
+        self.textbox.grid(row=1, column=1, padx=(20, 0), pady=(10, 0), sticky="nsew")
+        
         # set default values
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
-        # self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -86,10 +74,22 @@ class App(customtkinter.CTk):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
-    def sidebar_button_event(self):
-        print("sidebar_button click")
+    def start_button_event(self):
+        # start locate_auto_depth routine
+        print("start")
+        self.textbox.insert("0.0", "\nRunning location program")
 
-
+    def stop_button_event(self):
+        # return to home
+        print("stop")
+        self.textbox.insert("0.0", "\nStopping")
+    
+    def display_photo():
+        pass
+    
+    
+    
+    
 if __name__ == "__main__":
     app = App()
     app.mainloop()
